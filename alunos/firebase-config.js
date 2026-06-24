04144c6698922826db304c868cb9160a33b6d30e
Andre ODF <clubemultitrader@gmail.com>
2026-06-24 10:46:44 +0900

    firebase-config.js

--- alunos/firebase-config.js
+++ alunos/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyCdOM7eXkFVwmZMUqyJ1cnv_vLAsPW04N0",
+  authDomain: "clube-multi-trader.firebaseapp.com",
+  projectId: "clube-multi-trader",
+  storageBucket: "clube-multi-trader.firebasestorage.app",
+  messagingSenderId: "186379665921",
+  appId: "1:186379665921:web:5c9295d5e9246093d63600"
 };
 
 const app = initializeApp(firebaseConfig);
