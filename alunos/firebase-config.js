d856d0c8805aae9bf38edc4c3dcd4698c16dc4f5
Andre ODF <clubemultitrader@gmail.com>
2026-06-24 10:41:11 +0900

    firebase-config.js

--- alunos/firebase-config.js
+++ alunos/firebase-config.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
